28887f72d24ad902619ed62598ceec4ff1c95d49
alexandr <k4t4kun@gmail.com>
2023-03-11 13:49:28 +0100

    HW 9.1 done

--- src/ru/javawebinar/basejava/MainFile.java
+++ src/ru/javawebinar/basejava/MainFile.java
@@ -10,17 +10,22 @@ import java.io.IOException;
  */
 public class MainFile {
     public static void main(String[] args) throws IOException {
-
-        printAllFilesInDirectory("./basejava");
-
+        printAllFilesInDirectory("./basejava/src");
     }
 
-    private static void printAllFilesInDirectory(String filePath) {
-        File x = new File(filePath);
-        File[] files = x.listFiles();
+    private static void printAllFilesInDirectory(String filePath) throws IOException {
+        File directory = new File(filePath);
+        File[] files = directory.listFiles();
         if (files != null) {
             for (File file : files) {
+                int deep = file.getAbsolutePath().split("\\\\").length -
+                        file.getPath().split("\\\\").length;
+                int steps = file.getPath().split("\\\\").length - deep;
+                for (int i = 0; i <= steps; i++) {
+                    System.out.print(" . ");
+                }
                 if (file.isDirectory()) {
+                    System.out.println("\u25BC [" + file.getName() + "]");
                     printAllFilesInDirectory(file.getPath());
                 } else {
                     System.out.println(file.getName());
