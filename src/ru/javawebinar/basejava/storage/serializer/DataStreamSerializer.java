3d5e3075787941af9dd9c26b8b2dca4095abf480
alexandr <k4t4kun@gmail.com>
2023-04-03 18:18:32 +0200

    lesson 10, doWrite refactor, all test passed

--- src/ru/javawebinar/basejava/storage/serializer/DataStreamSerializer.java
+++ src/ru/javawebinar/basejava/storage/serializer/DataStreamSerializer.java
@@ -6,51 +6,63 @@ import ru.javawebinar.basejava.model.*;
 import java.io.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 
 public class DataStreamSerializer implements StreamSerializer {
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
+    @FunctionalInterface
+    public interface DosWriter<T> {
+        void write(T t) throws IOException;
+    }
+
+    private void writeWithException(Collection c, DataOutputStream dos, DosWriter writer) throws IOException {
+        dos.writeInt(c.size());
+        Objects.requireNonNull(writer);
+        for (Object o : c) {
+            writer.write(o);
+        }
+    }
+
     @Override
     public void doWrite(Resume r, OutputStream os) throws IOException {
+
         try (DataOutputStream dos = new DataOutputStream(os)) {
             dos.writeUTF(r.getUuid());
             dos.writeUTF(r.getFullName());
             Map<ContactType, String> contacts = r.getContacts();
-            dos.writeInt(contacts.size());
-            for (Map.Entry<ContactType, String> entry : contacts.entrySet()) {
-                dos.writeUTF(entry.getKey().name());
-                dos.writeUTF(entry.getValue());
-            }
+            writeWithException(contacts.entrySet(), dos, (o) -> {
+                Map.Entry entry = (Map.Entry) o;
+                Enum key = (Enum) entry.getKey();
+                dos.writeUTF(key.name());
+                dos.writeUTF((String) entry.getValue());
+            });
+
             // TODO implements sections
             Map<SectionType, Section> sections = r.getSections();
-            dos.writeInt(sections.size());
-            for (Map.Entry<SectionType, Section> entry : sections.entrySet()) {
-                String sectionTypeName = entry.getKey().name();
-                dos.writeUTF(sectionTypeName);
-                switch (sectionTypeName) {
-                    case "OBJECTIVE":
-                    case "PERSONAL":
+
+            writeWithException(sections.entrySet(), dos, (o) -> {
+                Map.Entry entry = (Map.Entry) o;
+                SectionType sectionType = (SectionType) entry.getKey();
+                dos.writeUTF(sectionType.name());
+                switch (sectionType) {
+                    case OBJECTIVE:
+                    case PERSONAL:
                         dos.writeUTF(((TextSection) entry.getValue()).getContent());
                         break;
-                    case "ACHIEVEMENT":
-                    case "QUALIFICATIONS":
+                    case ACHIEVEMENT:
+                    case QUALIFICATIONS:
                         ListSection listSection = (ListSection) entry.getValue();
-                        List<String> listStrings = listSection.getItems();
-                        dos.writeInt(listStrings.size());
-                        for (String s : listStrings) {
-                            dos.writeUTF(s);
-                        }
+                        List<String> stringList = listSection.getItems();
+                        writeWithException(stringList, dos, item -> dos.writeUTF((String) item));
                         break;
-                    case "EXPERIENCE":
-                    case "EDUCATION":
+                    case EXPERIENCE:
+                    case EDUCATION:
                         OrganizationSection organizationSection = (OrganizationSection) entry.getValue();
-                        List<Organization> listOrganizations = organizationSection.getOrganizations();
-                        dos.writeInt(listOrganizations.size());
-                        for (Organization org : listOrganizations) {
+                        List<Organization> organizationList = organizationSection.getOrganizations();
+                        writeWithException(organizationList, dos, o1 -> {
+                            Organization org = (Organization) o1;
                             dos.writeUTF(org.getHomePage().getName());
                             if (org.getHomePage().getUrl() != null) {
                                 dos.writeBoolean(true);
@@ -58,23 +70,73 @@ public class DataStreamSerializer implements StreamSerializer {
                             } else {
                                 dos.writeBoolean(false);
                             }
-                            List<Organization.Position> listPos = org.getPositions();
-                            dos.writeInt(listPos.size());
-                            for (Organization.Position p : listPos) {
-                                dos.writeUTF(p.getStartDate().format(formatter));
-                                dos.writeUTF(p.getEndDate().format(formatter));
-                                dos.writeUTF(p.getTitle());
-                                if (p.getDescription() != null) {
+                            List<Organization.Position> positionList = org.getPositions();
+                            writeWithException(positionList, dos, p -> {
+                                Organization.Position position = (Organization.Position) p;
+                                dos.writeUTF(position.getStartDate().format(formatter));
+                                dos.writeUTF(position.getEndDate().format(formatter));
+                                dos.writeUTF(position.getTitle());
+                                if (position.getDescription() != null) {
                                     dos.writeBoolean(true);
-                                    dos.writeUTF(p.getDescription());
+                                    dos.writeUTF(position.getDescription());
                                 } else {
                                     dos.writeBoolean(false);
                                 }
-                            }
-                        }
-                        break;
+                            });
+                        });
                 }
-            }
+            });
+
+
+//            dos.writeInt(sections.size());
+//            for (Map.Entry<SectionType, Section> entry : sections.entrySet()) {
+//                String sectionName = entry.getKey().name();
+//                dos.writeUTF(sectionName);
+//                SectionType sectionType = SectionType.valueOf(sectionName);
+//                switch (sectionType) {
+//                    case OBJECTIVE:
+//                    case PERSONAL:
+//                        dos.writeUTF(((TextSection) entry.getValue()).getContent());
+//                        break;
+//                    case ACHIEVEMENT:
+//                    case QUALIFICATIONS:
+//                        ListSection listSection = (ListSection) entry.getValue();
+//                        List<String> listStrings = listSection.getItems();
+//                        dos.writeInt(listStrings.size());
+//                        for (String s : listStrings) {
+//                            dos.writeUTF(s);
+//                        }
+//                        break;
+//                    case EXPERIENCE:
+//                    case EDUCATION:
+//                        OrganizationSection organizationSection = (OrganizationSection) entry.getValue();
+//                        List<Organization> listOrganizations = organizationSection.getOrganizations();
+//                        dos.writeInt(listOrganizations.size());
+//                        for (Organization org : listOrganizations) {
+//                            dos.writeUTF(org.getHomePage().getName());
+//                            if (org.getHomePage().getUrl() != null) {
+//                                dos.writeBoolean(true);
+//                                dos.writeUTF(org.getHomePage().getUrl());
+//                            } else {
+//                                dos.writeBoolean(false);
+//                            }
+//                            List<Organization.Position> listPos = org.getPositions();
+//                            dos.writeInt(listPos.size());
+//                            for (Organization.Position p : listPos) {
+//                                dos.writeUTF(p.getStartDate().format(formatter));
+//                                dos.writeUTF(p.getEndDate().format(formatter));
+//                                dos.writeUTF(p.getTitle());
+//                                if (p.getDescription() != null) {
+//                                    dos.writeBoolean(true);
+//                                    dos.writeUTF(p.getDescription());
+//                                } else {
+//                                    dos.writeBoolean(false);
+//                                }
+//                            }
+//                        }
+//                        break;
+//                }
+//            }
         } catch (IOException e) {
             throw new StorageException("DataOutputStream error", r.getUuid(), e);
         }
